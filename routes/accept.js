f21106512e74961a79aec5173d6b44218b28f853
james <happer@hotmail.com>
2015-07-07 09:10:09 -0400

    going into http request

--- routes/accept.js
+++ routes/accept.js
@@ -56,8 +56,6 @@ router.get('/', function (req, res) {
         }
     };
 
-/*
-
     var postRequest = http.request(requestOptions, function (acceptResponse) {
         var response = [];
         acceptResponse.setEncoding('utf8');
@@ -69,80 +67,85 @@ router.get('/', function (req, res) {
             //should include "created", "slug", "email", "expires", "issuedOn", "assertionUrl"
             var awardData = JSON.parse(response.join(''));
             console.log('accept response: ' + response.join(''));//output all at once
-            if (awardData.status === "created") { //status created
-                res.render('accept', {});
-
-                //mark application as processed
-                processApplication(badgeSlug, application);//we will add this next
-
-                function processApplication(bdgSlug, appSlug) {
-                    //process application
-                    var processPath = "/systems/badgekit/badges/" + bdgSlug + "/applications/" + appSlug;
-                    var proc = new Date();
-                    var processData = qs.stringify({
-                        processed: proc
-                    });
-                    var claimData = {
-                        header: { typ: 'JWT', alg: 'HS256' },
-                        payload: {
-                            key: 'master',
-                            exp: Date.now() + (1000 * 60),
-                            method: 'PUT',
-                            path: processPath,
-                            body: {
-                                alg: "SHA256",
-                                hash: crypto.createHash('SHA256').update(processData).digest('hex')
+/*
+                        if (awardData.status === "created") { //status created
+                            res.render('accept', {});
+            
+                            //mark application as processed
+                            processApplication(badgeSlug, application);//we will add this next
+            
+                            function processApplication(bdgSlug, appSlug) {
+                                //process application
+                                var processPath = "/systems/badgekit/badges/" + bdgSlug + "/applications/" + appSlug;
+                                var proc = new Date();
+                                var processData = qs.stringify({
+                                    processed: proc
+                                });
+                                var claimData = {
+                                    header: { typ: 'JWT', alg: 'HS256' },
+                                    payload: {
+                                        key: 'master',
+                                        exp: Date.now() + (1000 * 60),
+                                        method: 'PUT',
+                                        path: processPath,
+                                        body: {
+                                            alg: "SHA256",
+                                            hash: crypto.createHash('SHA256').update(processData).digest('hex')
+                                        }
+                                    },
+                                    secret: 'donttell'
+                                };
+            
+                                var requestOptions = {
+                                    host: 'gentle-headland-3091.herokuapp.com',
+                                    path: processPath,
+                                    method: 'PUT',
+                                    headers: {
+                                        'Authorization': 'JWT token="' + jws.sign(claimData) + '"',
+                                        'Content-Type': 'application/x-www-form-urlencoded',
+                                        'Content-Length': Buffer.byteLength(processData)
+                                    }
+                                };
+            
+                                var putRequest = http.request(requestOptions, function (processResponse) {
+                                    var response = [];
+                                    processResponse.setEncoding('utf8');
+                                    processResponse.on('data', function (responseData) {
+                                        response.push(responseData);
+                                    });
+                                    processResponse.on('end', function () {
+                                        console.log("process response: " + response.join(''));
+                                    });
+                                });
+            
+                                putRequest.on('error', function (e) {
+                                    console.error(e);
+                                });
+                                // post the data--this should update the application as processed so
+                                // so it should no longer show up on the review page
+                                putRequest.write(processData);
+                                putRequest.end();
                             }
-                        },
-                        secret: 'donttell'
-                    };
-
-                    var requestOptions = {
-                        host: 'gentle-headland-3091.herokuapp.com',
-                        path: processPath,
-                        method: 'PUT',
-                        headers: {
-                            'Authorization': 'JWT token="' + jws.sign(claimData) + '"',
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                            'Content-Length': Buffer.byteLength(processData)
                         }
-                    };
+                        else {
+                            res.send("Whoops! Something went wrong with your badge.");
+                        }
 
-                    var putRequest = http.request(requestOptions, function (processResponse) {
-                        var response = [];
-                        processResponse.setEncoding('utf8');
-                        processResponse.on('data', function (responseData) {
-                            response.push(responseData);
-                        });
-                        processResponse.on('end', function () {
-                            console.log("process response: " + response.join(''));
-                        });
-                    });
-
-                    putRequest.on('error', function (e) {
-                        console.error(e);
-                    });
-                    // post the data--this should update the application as processed so
-                    // so it should no longer show up on the review page
-                    putRequest.write(processData);
-                    putRequest.end();
-                }
-            }
-            else {
-                res.send("Whoops! Something went wrong with your badge.");
-            }
-        });
-    });
-
-    postRequest.on('error', function (e) {
-        console.error(e);
-    });
-    // post the data--I believe this leads to the creation of the badge instance
-    postRequest.write(awardData);
-    postRequest.end();
 */
 
-    console.log("accept.js accepted to this point");
+                    });
+                });
+            
+/*
+
+                postRequest.on('error', function (e) {
+                    console.error(e);
+                });
+                // post the data--I believe this leads to the creation of the badge instance/                postRequest.write(awardData);
+                postRequest.end();
+*/
+            console.log("accept.js accepted to this point");
+        
 });
 
 
