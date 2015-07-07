ee2e2e4829a3d59e217e879f41138ec24c7de30f
james <happer@hotmail.com>
2015-07-07 15:03:18 -0400

    asdfd

--- routes/accept.js
+++ routes/accept.js
@@ -42,7 +42,7 @@ router.get('/', function (req, res) {
                 hash: crypto.createHash('sha256').update(awardData).digest('hex')
             }
         },
-        secret: 'zither'
+        secret: 'donttell'
     };
 
     var requestOptions = {
